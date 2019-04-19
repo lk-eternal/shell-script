0ac3adc183371f3aa402d6b32d0011a3e948f584
arc <arkceajin@gmail.com>
2019-04-19 17:04:14 +0900

    Update compile_opencv.sh

--- compile_opencv.sh
+++ compile_opencv.sh
@@ -1,5 +1,5 @@
-cd ~ && wget https://github.com/Kitware/CMake/releases/download/v3.5.1/cmake-3.5.1-Linux-x86_64.sh
-chmod a+x cmake-3.5.1-Linux-x86_64.sh
+sudo cd /opt && wget https://github.com/Kitware/CMake/releases/download/v3.5.1/cmake-3.5.1-Linux-x86_64.sh
+sudo chmod a+x cmake-3.5.1-Linux-x86_64.sh
 sudo bash cmake-3.5.1-Linux-x86_64.sh
 sudo ln -s /opt/cmake-3.5.1-Linux-x86_64/bin/* /usr/local/bin
 
