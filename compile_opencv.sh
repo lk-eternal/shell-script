9caeb4e52da235c9e7382127d491eae15aebd526
arc <arkceajin@gmail.com>
2019-04-19 17:18:42 +0900

    Update compile_opencv.sh

--- compile_opencv.sh
+++ compile_opencv.sh
@@ -1,4 +1,4 @@
-sudo cd /opt && wget https://github.com/Kitware/CMake/releases/download/v3.5.1/cmake-3.5.1-Linux-x86_64.sh
+cd /opt && sudo wget https://github.com/Kitware/CMake/releases/download/v3.5.1/cmake-3.5.1-Linux-x86_64.sh
 sudo chmod a+x cmake-3.5.1-Linux-x86_64.sh
 sudo bash cmake-3.5.1-Linux-x86_64.sh
 sudo ln -s /opt/cmake-3.5.1-Linux-x86_64/bin/* /usr/local/bin
@@ -10,5 +10,5 @@ cd opencv-4.0.1
 mkdir build
 cd build
 
-cmake -DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake ../
+cmake -DCMAKE_C_COMPILER=/usr/bin/arm-linux-gnueabihf-gcc-4.9 -DCMAKE_CXX_COMPILER=/usr/bin/arm-linux-gnueabihf-g++-4.9 ../
 make -j 4
