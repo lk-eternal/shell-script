623f8f74e33a46c70bbbae11819f3b4a6582cb23
LK <49933329+lk-eternal@users.noreply.github.com>
2019-04-25 18:40:58 +0900

    Update wsl-4.9-arm-linux-gnueabihf_install.sh

--- wsl-4.9-arm-linux-gnueabihf_install.sh
+++ wsl-4.9-arm-linux-gnueabihf_install.sh
@@ -3,10 +3,11 @@ sudo touch /etc/apt/sources.list.d/crosstools.list
 sudo chmod a+w /etc/apt/sources.list.d/crosstools.list
 sudo echo "deb http://emdebian.org/tools/debian/ jessie main" > /etc/apt/sources.list.d/crosstools.list
 
+sudo apt-get -y install curl
 curl http://emdebian.org/tools/debian/emdebian-toolchain-archive.key | sudo apt-key add -
 sudo dpkg --add-architecture armhf
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get install gcc-4.9-arm-linux-gnueabihf
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install gcc-4.9-arm-linux-gnueabihf
 sudo reboot
