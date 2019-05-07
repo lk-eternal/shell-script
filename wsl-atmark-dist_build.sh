0c467dc317576fa7b7b7fb6ee8173ef878cf55d1
LK <49933329+lk-eternal@users.noreply.github.com>
2019-05-07 11:20:33 +0900

    Update wsl-atmark-dist_build.sh

--- wsl-atmark-dist_build.sh
+++ wsl-atmark-dist_build.sh
@@ -4,7 +4,7 @@ sudo apt-get install build-essential
 wget --no-check-certificate https://users.atmark-techno.com/files/downloads/armadillo-840/source/atmark-dist-20180330.tar.gz
 tar xzf atmark-dist-20180330.tar.gz
 
-wget --no-check-certificate https://users.atmark-techno.com/files/downloads/armadillo-840/source/linux-3.4-at26.tar.gz
+wget --no-check-certificate https://download.atmark-techno.com/kernel-source/linux-3.4-at/linux-3.4-at16.tar.gz
 tar xzf linux-3.4-at26.tar.gz
 ln -s ../linux-3.4-at26 atmark-dist-20180330/linux-3.x
 cd atmark-dist-20180330
