23341e306596cf8d9e221df99b87d8035a6f2ffa
LK <49933329+lk-eternal@users.noreply.github.com>
2019-05-07 09:55:27 +0900

    Update upgrade-debian.sh

--- upgrade-debian.sh
+++ upgrade-debian.sh
@@ -1,8 +1,16 @@
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.wheezy
-sudo sed -i 's/wheezy/jessie/g' /etc/apt/sources.list
+#sudo sed -i 's/wheezy/jessie/g' /etc/apt/sources.list
+wget --no-check-certificate https://raw.githubusercontent.com/arkceajin/shell-script/master/sources.list
+sudo cp -f sources.list /etc/apt/sources.list
+
 sudo touch /etc/apt/sources.list.d/crosstools.list
+sudo touch /etc/apt/sources.list.d/atmark-techno.list
 sudo chmod a+w /etc/apt/sources.list.d/crosstools.list
+sudo chmod a+w /etc/apt/sources.list.d/atmark-techno.list
 sudo echo "deb http://emdebian.org/tools/debian/ jessie main" > /etc/apt/sources.list.d/crosstools.list
+sudo echo "deb http://download.atmark-techno.com/debian/ jessie main contrib non-free" > /etc/apt/sources.list.d/atmark-techno.list
+
+sudo apt-get install -y curl
 curl http://emdebian.org/tools/debian/emdebian-toolchain-archive.key | sudo apt-key add -
 sudo dpkg --add-architecture armel
 sudo apt-get update
