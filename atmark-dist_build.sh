e9c6f30323c10abd6009659823b22127b5dad261
LK <49933329+lk-eternal@users.noreply.github.com>
2019-05-08 17:57:04 +0900

    Update atmark-dist_build.sh

--- atmark-dist_build.sh
+++ atmark-dist_build.sh
@@ -22,7 +22,26 @@ sudo apt-get install -y zlib1g-dev:armhf
 sudo apt-get install -y bison
 sudo apt-get install -y flex
 sudo apt-get install -y glib-2.0
+
+sudo apt-get install -y libperl-dev
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y liboobs-1-dev
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y glib-2.0:armhf
+
+sudo cp /usr/lib/i386-linux-gnu/libgobject-2.0.* /usr/lib/arm-linux-gnueabihf/
+
+export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig/
+
+~/atmark-dist-20180330/user/gstreamer/gstreamer1.0/gstreamer1.0-1.0.8/gst/parse/grammar.y
+-int priv_gst_parse_yylex (void * yylval_param , yyscan_t yyscanner);
++int priv_gst_parse_yylex (void * yylval_param);
+
+~/atmark-dist-20180330/user/gstreamer/gstreamer1.0/build/gst/parse/lex.priv_gst_parse_yy.c
+1022
+//int priv_gst_parse_yyget_column  (yyscan_t yyscanner );
+//void priv_gst_parse_yyset_column (int column_no ,yyscan_t yyscanner );
 
 sed -i "s?#include <sys/mman.h>?#include <sys/mman.h>\n#include <sys/resource.h>?" ./user/busybox/busybox-1.20.2/include/libbb.h
 
